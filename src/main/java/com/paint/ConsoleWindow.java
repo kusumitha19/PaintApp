6278e413f5d6b00e21d4c57dca5c40214c4575df
mallapalligagana@gmail.com <mallapalligagana@gmail.com>
2026-03-26 21:22:57 +0530

    second commit added console window and dialogBox

---
+++ src/main/java/com/paint/ConsoleWindow.java
@@ -0,0 +1,18 @@
+package com.paint;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import com.paint.Window;
+
+/**
+ *
+ * @author 1da20
+ */
+public class ConsoleWindow extends Window {
+     public void showConsole() {
+        System.out.println("Console window");
+    }
+}
